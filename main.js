1bc26d29e828618a895bbf204614509843235dd7
Juan proyect WS <montoj@gmail.com>
2022-06-28 11:53:27 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+const inputCard = document.querySelector('#inputCard');
+const inputDate = document.querySelector('#inputDate');
+const inputCVV = document.querySelector('#inputCVV');
+
+const maskCard = '####-####-####-####';
+const maskDate = '##/##';
+const maskCVV = '###';
+
+let current = '';
+let cardNumber = [];
+let dateNumber = [];
+let cvvNumber = [];
+
+inputCard.addEventListener('keydown', e => {
+	if (e.key == 'Tab') {
+		return;
+	}
+	e.preventDefault();
+
+	handleInput(maskNumber, e.key, cardNumber);
+});
