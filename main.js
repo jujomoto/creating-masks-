59ce496d3fb657d6bcff5fc927ee5100a212b379
Juan proyect WS <montoj@gmail.com>
2022-06-29 14:40:33 -0400

    adding new changes

--- main.js
+++ main.js
@@ -2,20 +2,31 @@ const inputCard = document.querySelector('#inputCard');
 const inputDate = document.querySelector('#inputDate');
 const inputCVV = document.querySelector('#inputCVV');
 
-const maskCard = '####-####-####-####';
+const maskNumber = '####-####-####-####'; //length: 19
 const maskDate = '##/##';
 const maskCVV = '###';
 
 let current = '';
-let cardNumber = [];
-let dateNumber = [];
-let cvvNumber = [];
+let arrCardNumber = [];
+let arrDateNumber = [];
+let arrCvvNumber = [];
 
 inputCard.addEventListener('keydown', e => {
 	if (e.key == 'Tab') {
 		return;
 	}
 	e.preventDefault();
+	console.log(e);
 
-	handleInput(maskNumber, e.key, cardNumber);
+	handleInput(maskNumber, e.key, arrCardNumber);
+	inputCard.value = arrCardNumber.join('');
+
 });
+
+function handleInput (mask, key, arr) {
+	 let numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+	 if (key === 'Backspace' && arr.lenght > 0) {
+		arr.pop();
+		return;
+	 }
+}
